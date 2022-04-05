4971e6f4831401f395a6388c299361583c8f4599
Jarod Wilson <jaroddwilson@gmail.com>
2022-04-04 21:46:55 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([])
+  const [editPizza, setEditPizza] = useState({})
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+    .then(r => r.json())
+    .then(data => setPizzas(data))
+  }, [])
+
+  function handleEditPopulate(pizza) {
+    setEditPizza(pizza)
+  }
+
+  function handleChangePizza(name, value) {
+    setEditPizza({...editPizza, [name]: name==="vegetarian" ? value==="Vegetarian" : value })
+  }
+
+  function handleSubmitPizza() {
+    fetch(`http://localhost:3001/pizzas/${editPizza.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editPizza)
+    })
+    .then(r => r.json())
+    .then(data => {
+      const newPizzaData = [...pizzas];
+      const found = newPizzaData.find(p => p.id===data.id);
+      const index = newPizzaData.indexOf(found);
+      newPizzaData[index] = data;
+      setPizzas(newPizzaData);
+      setEditPizza({id: 0, topping: "", size: "Small", vegetarian: undefined});
+    })
+  }
+
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm editPizza={editPizza} onChange={handleChangePizza} onSubmit={handleSubmitPizza} />
+      <PizzaList pizzas={pizzas} onEditPizza={handleEditPopulate} />
     </>
   );
 }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-function PizzaForm() {
+function PizzaForm({ editPizza, onChange, onSubmit }) {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit();
+  }
+
+  function handleChange(e) {
+    onChange(e.target.name, e.target.value)
+  }
+
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="col-5">
           <input
@@ -10,10 +20,12 @@ function PizzaForm() {
             type="text"
             name="topping"
             placeholder="Pizza Topping"
+            value={editPizza.topping}
+            onChange={handleChange}
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select className="form-control" name="size" value={editPizza.size} onChange={handleChange}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -26,6 +38,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Vegetarian"
+              checked={editPizza.vegetarian===true}
+              onChange={handleChange}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -35,6 +49,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              checked={editPizza.vegetarian===false}
+              onChange={handleChange}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
